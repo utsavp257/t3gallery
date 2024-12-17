c3015dc90b99b64036a71517b5ad05e7ec806516
utsavp257 <patelutsav257@gmail.com>
2024-12-18 00:08:08 +0530

    database integegration with UI

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [x] Make it deploy (vercel)
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [ ] Actually set up a database (vercel postgres)
+- [x] Actually set up a database (vercel postgres)
 - [ ] Attach database to UI
 - [ ] Add authentication (w/ clerk)
 - [ ] Add image upload
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,29 +18,23 @@ const mockUrls = [
   "https://utfs.io/f/LxqRyZPzYaceiADlaNP5TFqtOD7z1A9vQ6j5aKWUIgLSb3Rr"
 ];
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
  
   //console.log(posts);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>
-            {post.name}
-          </div>
-        ))}
         {
-          mockImages.map((image) => (
-            <div key={image.id} className="w-48"> 
+          images.map((image) => (
+            <div key={image.id} className="flex w-48 flex-col"> 
               <img src={image.url} />
+              <div>{image.name}</div>
             </div>
           ))
         }
