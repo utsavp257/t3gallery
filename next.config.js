f195a81b5258fa11a6d47791f13c7f0560f390bb
utsavp257 <patelutsav257@gmail.com>
2024-12-18 19:01:13 +0530

    sentry setup done

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {
+const coreConfig = {
     images: {
         remotePatterns: [
             {hostname: "utfs.io"}
@@ -19,5 +19,40 @@ const config = {
     },
     
 };
+
+import { withSentryConfig } from "@sentry/nextjs";
+
+const config = withSentryConfig(
+  coreConfig,
+  {
+    org: "utsav-gs",
+    project: "t3-gallery",
+    silent: !process.env.CI,
+  },
+  //@ts-ignore
+  {
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
+);
 
 export default config;
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a2776f82d0880fdab3100e6826ddbf15@o4508489240084480.ingest.us.sentry.io/4508489250242560",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
