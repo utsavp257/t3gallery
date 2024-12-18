540b73dbb5bbabb50dd9f4b4377698d9c75b87ce
utsavp257 <patelutsav257@gmail.com>
2024-12-19 01:20:19 +0530

    better modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -5,7 +5,24 @@ export default async function FullPageImageView(props: {id: number}) {
 
   return (
 
-      <img src={image.url} className="w-96" />
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+    <div className="flex-shrink flex-grow">
+      <img src={image.url} className="object-contain" alt={image.name} />
+    </div>
+    <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
+      <div className="border-b p-2 text-center text-xl">{image.name}</div>
+
+      <div className="p-2">
+        <div>Uploaded By:</div>
+        {/* { <div>{userInfo.fullName}</div> } */}
+      </div>
+
+      <div className="p-2">
+        <div>Created On:</div>
+        <div>{image.createdAt.toLocaleDateString()}</div>
+      </div>
+    </div>
+  </div>
 
   );
 }
