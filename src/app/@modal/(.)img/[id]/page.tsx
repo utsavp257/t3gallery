d9b7d83f64a685b594c21e64151e4894ef679b3c
utsavp257 <patelutsav257@gmail.com>
2024-12-19 01:07:17 +0530

    full screen dialog box for images, modal and pages

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { getImage } from "~/server/queries";
+import { Modal } from "./modal";
+import FullPageImageView from "~/components/full-image-page";
 
 export default async function PhotoModal({   //MODAL CODE
     params,
@@ -10,11 +12,10 @@ export default async function PhotoModal({   //MODAL CODE
       const idAsNumber = Number(photoId);
       if(Number.isNaN(idAsNumber)) throw new Error("Invalid photo ID");
 
-      const image = await getImage(idAsNumber);
       return (
-        <div>
-          <img src={image.url} className="w-96" />
-        </div>
+        <Modal>
+          <FullPageImageView id={idAsNumber} />
+        </Modal>
       );
     } catch (error) {
       console.error("Error resolving params:", error);
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,14 +1,21 @@
-export default async function PhotoModal({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  try {
-    const { id: photoId } = await params; // Await and destructure
-    console.log(photoId); // Debugging log
-    return <div>{photoId}</div>;
-  } catch (error) {
-    console.error("Error resolving params:", error);
-    return <div>Error loading photo</div>;
+
+import FullPageImageView from "~/components/full-image-page";
+
+export default async function PhotoPage({   //PAGE CODE
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  }) {
+    try {
+      const { id: photoId } = await params; // Await and destructure
+      const idAsNumber = Number(photoId);
+      if(Number.isNaN(idAsNumber)) throw new Error("Invalid photo ID");
+
+      return (
+          <FullPageImageView id={idAsNumber} />
+      );
+    } catch (error) {
+      console.error("Error resolving params:", error);
+      return <div>Error loading photo</div>;
+    }
   }
-}
