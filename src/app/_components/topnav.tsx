0725c81f5d8056e40c8774f2ee41e4720b2588d9
utsavp257 <patelutsav257@gmail.com>
2024-12-18 16:38:32 +0530

    uploadthing still an issue

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,17 +1,29 @@
+"use client";
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import { Sign } from "crypto";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav(){
+
+    const router = useRouter();
+
   return(
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>
         Gallery
       </div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
            <SignInButton/>
         </SignedOut>
         <SignedIn>
+            <UploadButton endpoint="imageUploader"
+              onClientUploadComplete={(res) => {
+              console.log("Client upload complete:", res);
+              router.refresh();
+            }}
+            />
             <UserButton/>        
         </SignedIn>
       </div>
---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,51 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { db } from "~/server/db";
+import { images } from "~/server/db/schema";
+
+const f = createUploadthing();
+
+
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 40 } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = auth();
+
+      // If you throw, the user will not be able to upload
+      if (!(await user).userId) throw new UploadThingError("Unauthorized");
+
+      // await db.insert(images).values({
+      //   name: '13.png',
+      //   url: 'https://utfs.io/f/LxqRyZPzYaceDe2baEtrgyw6FavhZo8LYUx1QnO09zBfsiH3',
+      //   userId: '231234',
+      // });
+      // console.log("db workedddd")
+      //  console.log( (await user).userId )
+       
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: (await user).userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      try {
+        console.log("onUploadComplete")
+        await db.insert(images).values({
+          name: file.name,
+          url: file.url,
+          userId: metadata.userId!,
+        });
+
+        return { uploadedBy: metadata.userId };
+      } catch (error) {
+        console.log(error);
+      }
+      
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
